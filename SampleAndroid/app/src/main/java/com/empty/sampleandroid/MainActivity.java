96d700b81a6c6d7c03d499d303e8ddc31631573f
Jeong Hun <wanted0301@gmail.com>
2020-01-30 05:40:21 +0900

    200130 update
    Android Tutorial Start!

---
+++ SampleAndroid/app/src/main/java/com/empty/sampleandroid/MainActivity.java
@@ -0,0 +1,15 @@
+package com.empty.sampleandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    // 자바의 main 메소드
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
